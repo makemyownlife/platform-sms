4f2ccd7425c5f0e1ca83aaa4f27edb5df11d08e5
zhangyong <zhangyong7120180@163.com>
2019-12-30 20:02:46 +0800

    first init

---
+++ src/main/java/com/courage/platform/sms/MainApplcation.java
@@ -0,0 +1,12 @@
+package com.courage.platform.sms;
+
+/**
+ * Created by zhangyong on 2019/12/30.
+ */
+public class MainApplcation {
+
+    public static void main(String[] args) {
+        System.out.println(12321);
+    }
+
+}
