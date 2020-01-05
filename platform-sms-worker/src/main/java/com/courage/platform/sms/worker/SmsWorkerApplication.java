0d00445102aa0c90b35ed12dcb949c95dc267a07
zhangyong <zhangyong7120180@163.com>
2020-01-05 16:22:32 +0800

    platform

--- platform-sms-worker/src/main/java/com/courage/platform/sms/worker/SmsWorkerApplication.java
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/SmsWorkerApplication.java
@@ -1,12 +1,19 @@
 package com.courage.platform.sms.worker;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Created by zhangyong on 2020/1/4.
  */
+@SpringBootApplication(scanBasePackages = "com.courage.platform.sms")
+@ServletComponentScan
+@EnableScheduling
 public class SmsWorkerApplication {
 
     public static void main(String[] args) {
-
+        
     }
 
 }
