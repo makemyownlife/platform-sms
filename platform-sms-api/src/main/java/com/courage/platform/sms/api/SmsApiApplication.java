caf9c9b51320312fb4d4b356642cb9f5d28609e5
zhangyong <zhangyong7120180@163.com>
2020-01-05 14:15:03 +0800

    first init application

--- platform-sms-api/src/main/java/com/courage/platform/sms/api/SmsApiApplication.java
+++ platform-sms-api/src/main/java/com/courage/platform/sms/api/SmsApiApplication.java
@@ -1,10 +1,20 @@
 package com.courage.platform.sms.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Created by zhangyong on 2020/1/4.
  */
+@SpringBootApplication(scanBasePackages = "com.courage.platform.sms")
+@ServletComponentScan
+@EnableScheduling
 public class SmsApiApplication {
 
-  // private final static
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApiApplication.class, args);
+    }
 
 }
