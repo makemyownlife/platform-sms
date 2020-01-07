87fb6fa5abadb774ae3e20cce915a750baf97744
zhangyong <zhangyong7120180@163.com>
2020-01-07 13:34:47 +0800

    sms api service

---
+++ platform-sms-api/src/main/java/com/courage/platform/sms/api/service/SmsApiService.java
@@ -0,0 +1,14 @@
+package com.courage.platform.sms.api.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmsApiService {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsApiService.class);
+
+
+
+}
