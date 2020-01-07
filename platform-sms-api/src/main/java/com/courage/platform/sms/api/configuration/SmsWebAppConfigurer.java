a0c9914597130c3734c2d88afd2a605aeb3fdc14
zhangyong <zhangyong7120180@163.com>
2020-01-07 10:44:28 +0800

    sms web configuration

--- platform-sms-api/src/main/java/com/courage/platform/sms/api/configuration/SmsWebAppConfigurer.java
+++ platform-sms-api/src/main/java/com/courage/platform/sms/api/configuration/SmsWebAppConfigurer.java
@@ -1,9 +1,13 @@
 package com.courage.platform.sms.api.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SmsWebAppConfigurer implements WebMvcConfigurer {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsWebAppConfigurer.class);
 
 }
