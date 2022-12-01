feb09ecfdc33bc016cf4644e9e0891ad31879036
zhangyong <zhangyong7120180@163.com>
2022-12-01 15:10:37 +0800

    [添加代码] 短信平台

--- platform-sms-admin/admin-web/main/java/com/alibaba/otter/canal/admin/controller/UserController.java
+++ platform-sms-admin/admin-web/main/java/com/alibaba/otter/canal/admin/controller/UserController.java
@@ -3,12 +3,6 @@ package com.alibaba.otter.canal.admin.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 用户管理控制层
- *
- * @author rewerma 2019-07-13 下午05:12:16
- * @version 1.0.0
- */
 @RestController
 @RequestMapping("/api/{env}/user")
 public class UserController {
