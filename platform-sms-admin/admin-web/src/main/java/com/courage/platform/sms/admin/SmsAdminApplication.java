a551b89ffebc78c5cf1231fc63cf1f13552ff583
makemyownlife <zhangyong7120180@163.com>
2022-12-03 18:20:03 +0800

    [添加代码] platform-ui 依赖

--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/SmsAdminApplication.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/SmsAdminApplication.java
@@ -1,14 +1,20 @@
 package com.courage.platform.sms.admin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmsAdminApplication {
 
+    private final static Logger logger = LoggerFactory.getLogger(SmsAdminApplication.class);
+
     public static void main(String[] args) {
+        logger.info("开始启动短信平台管理服务");
         SpringApplication application = new SpringApplication(SmsAdminApplication.class);
         application.run(args);
+        logger.info("结束启动短信平台管理服务");
     }
 
 }
