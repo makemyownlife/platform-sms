24159058bdff20a05075f503f5a24c16c4c5c0a4
zhangyong <zhangyong7120180@163.com>
2022-12-05 11:46:17 +0800

    [添加代码] 短信平台擅长多余的文件

--- platform-sms-server/src/main/java/com/courage/platform/sms/server/SmsServerApplication.java
+++ platform-sms-server/src/main/java/com/courage/platform/sms/server/SmsServerApplication.java
@@ -3,7 +3,6 @@ package com.courage.platform.sms.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
