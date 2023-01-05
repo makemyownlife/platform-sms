790dbb7ee89c26a85a1d2bb9e2a7e958c57e19e7
zhangyong <zhangyong7120180@163.com>
2023-01-05 17:29:53 +0800

    [添加代码] 消费者 worker 启动信息

---
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/job/SmsMessageConsumer.java
@@ -0,0 +1,25 @@
+package com.courage.platform.sms.worker.job;
+
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhangyong on 2023/1/5.
+ */
+@Component
+@RocketMQMessageListener(consumerGroup = "${rocketmq.consumer.group}",  // 消费组，格式：namespace全称%group名称
+        // 需要使用topic全称，所以进行topic名称的拼接，也可以自己设置  格式：namespace全称%topic名称
+        topic = "${rocketmq.consumer.topic}")
+public class SmsMessageConsumer implements RocketMQListener<String> {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsMessageConsumer.class);
+
+    @Override
+    public void onMessage(String message) {
+        logger.info("message:" + message);
+    }
+
+}
