a16f95077e2e7386e668254e730eb361f2c73b7a
zhangyong <zhangyong7120180@163.com>
2023-01-12 16:00:54 +0800

    [添加代码] 应用加载到缓存中。信息优化

--- platform-sms-dao/src/main/java/com/courage/platform/sms/dao/TSmsAppinfoDAO.java
+++ platform-sms-dao/src/main/java/com/courage/platform/sms/dao/TSmsAppinfoDAO.java
@@ -4,9 +4,13 @@ import com.courage.platform.sms.domain.TSmsAppinfo;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TSmsAppinfoDAO extends MyBatisBaseDao<TSmsAppinfo, Long> {
 
     TSmsAppinfo getAppinfoByAppKey(@Param("appKey") String appKey);
+
+    List<TSmsAppinfo> selectAll();
 
 }
