8022dc36e858cd5c5c9b2c7f40792c8207f24b3c
makemyownlife <zhangyong7120180@163.com>
2023-01-21 21:35:44 +0800

    [添加代码] 添加阿里云短信

--- platform-sms-adapter/adapter-alipay/src/main/java/com/courage/platform/sms/adapter/alipay/AlipayAdapter.java
+++ platform-sms-adapter/adapter-alipay/src/main/java/com/courage/platform/sms/adapter/alipay/AlipayAdapter.java
@@ -2,8 +2,12 @@ package com.courage.platform.sms.adapter.alipay;
 
 import com.courage.platform.sms.adapter.core.OuterAdapter;
 import com.courage.platform.sms.adapter.core.support.SPI;
+import com.google.gson.Gson;
 
 @SPI("emay")
 public class AlipayAdapter implements OuterAdapter {
 
+    public static void main(String[] args) {
+        
+    }
 }
