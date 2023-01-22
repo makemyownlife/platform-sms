ddde1b1807a31069d36de60f5393b6877da8a403
makemyownlife <zhangyong7120180@163.com>
2023-01-22 14:41:54 +0800

    [添加代码] 我们应该考虑信息

---
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterLoader.java
@@ -0,0 +1,12 @@
+package com.courage.platform.sms.worker.loader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmsAdapterLoader {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsAdapterLoader.class);
+
+
+
+}
---
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterService.java
@@ -0,0 +1,12 @@
+package com.courage.platform.sms.worker.loader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SmsAdapterService {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsAdapterService.class);
+    
+}
