2cdaee37389e86528f0065568a3f029b82ae063a
makemyownlife <zhangyong7120180@163.com>
2023-01-24 15:05:40 +0800

    [添加代码] 阿里云适配器信息

--- platform-sms-adapter/adapter-aliyun/src/main/java/com/courage/platform/sms/adapter/aliyun/AliyunAdapter.java
+++ platform-sms-adapter/adapter-aliyun/src/main/java/com/courage/platform/sms/adapter/aliyun/AliyunAdapter.java
@@ -1,4 +1,4 @@
-package com.courage.platform.sms.adapter.alicloud;
+package com.courage.platform.sms.adapter.aliyun;
 
 import com.aliyun.dysmsapi20170525.Client;
 import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
@@ -34,26 +34,5 @@ public class AliyunAdapter implements OuterAdapter {
         config.endpoint = "dysmsapi.aliyuncs.com";
         return new com.aliyun.dysmsapi20170525.Client(config);
     }
-
-    public static void main(String[] args) throws Exception {
-        // 工程代码泄露可能会导致AccessKey泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html
-        com.aliyun.dysmsapi20170525.Client client = createClient("accessKeyId", "accessKeySecret");
-        com.aliyun.dysmsapi20170525.models.SendSmsRequest sendSmsRequest = new com.aliyun.dysmsapi20170525.models.SendSmsRequest()
-                .setPhoneNumbers("your_value")
-                .setTemplateCode("mycode")
-                .setSignName("your_value");
-        try {
-            // 复制代码运行请自行打印 API 的返回值
-            SendSmsResponse smsResponse = client.sendSmsWithOptions(sendSmsRequest, new com.aliyun.teautil.models.RuntimeOptions());
-        } catch (TeaException error) {
-            // 如有需要，请打印 error
-            com.aliyun.teautil.Common.assertAsString(error.message);
-        } catch (Exception _error) {
-            TeaException error = new TeaException(_error.getMessage(), _error);
-            // 如有需要，请打印 error
-            com.aliyun.teautil.Common.assertAsString(error.message);
-        }
-    }
-
 
 }
