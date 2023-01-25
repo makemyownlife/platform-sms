4a218499ef87a9a910d583df8f3939a88f7ae630
makemyownlife <zhangyong7120180@163.com>
2023-01-25 21:22:11 +0800

    [添加代码] startup信息

--- platform-sms-worker/src/main/bin/startup.bat
+++ platform-sms-worker/src/main/bin/startup.bat
@@ -12,11 +12,11 @@ set CLASSPATH=%conf_dir%\..\lib\*;%CLASSPATH%
 
 set JAVA_MEM_OPTS= -Xms128m -Xmx512m -XX:PermSize=128m
 set JAVA_OPTS_EXT= -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dapplication.codeset=UTF-8 -Dfile.encoding=UTF-8
-set ADAPTER_OPTS= -DappName=canal-adapter
+set ADAPTER_OPTS= -DappName=sms-worker
 
 set JAVA_OPTS= %JAVA_MEM_OPTS% %JAVA_OPTS_EXT% %ADAPTER_OPTS%
 
-set CMD_STR= java %JAVA_OPTS% -classpath "%CLASSPATH%" com.alibaba.otter.canal.adapter.launcher.CanalAdapterApplication
+set CMD_STR= java %JAVA_OPTS% -classpath "%CLASSPATH%" com.courage.platform.sms.worker.SmsWorkerApplication
 echo start cmd : %CMD_STR%
 
-java %JAVA_OPTS% -classpath "%CLASSPATH%" com.alibaba.otter.canal.adapter.launcher.CanalAdapterApplication
+java %JAVA_OPTS% -classpath "%CLASSPATH%" com.courage.platform.sms.worker.SmsWorkerApplication
