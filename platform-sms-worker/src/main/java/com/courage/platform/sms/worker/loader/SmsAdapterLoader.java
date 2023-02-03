0fabc540a8207c927357d37a0e74866cfd37d04e
zhangyong <zhangyong7120180@163.com>
2023-02-03 16:02:37 +0800

    [修改代码] 适配器加载器

--- platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterLoader.java
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterLoader.java
@@ -2,10 +2,12 @@ package com.courage.platform.sms.worker.loader;
 
 import com.courage.platform.sms.adapter.OuterAdapter;
 import com.courage.platform.sms.adapter.support.ExtensionLoader;
-import com.courage.platform.sms.adapter.support.SmsChannelConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *  适配器加载器
+ */
 public class SmsAdapterLoader {
 
     private final static Logger logger = LoggerFactory.getLogger(SmsAdapterLoader.class);
@@ -13,17 +15,7 @@ public class SmsAdapterLoader {
     private ExtensionLoader<OuterAdapter> loader;
 
     public void init() throws Exception {
-        loader = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
-        OuterAdapter aliyunAdapter = loader.getExtension("emay");
-        SmsChannelConfig smsChannelConfig = new SmsChannelConfig();
-        aliyunAdapter.init(smsChannelConfig);
-        System.out.println(aliyunAdapter);
-    }
-
-    public static void main(String[] args) {
-        ExtensionLoader<OuterAdapter> loader = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
-        OuterAdapter outerAdapter = loader.getDefaultExtension();
-        System.out.println(outerAdapter);
+        this.loader = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
     }
 
 }
