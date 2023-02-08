2f694ec6ed5fd8123d7bcfeaef1053b45d6b2347
makemyownlife <zhangyong7120180@163.com>
2023-02-08 20:07:31 +0800

    [添加代码]  适配器加载

--- platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterLoader.java
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterLoader.java
@@ -2,6 +2,7 @@ package com.courage.platform.sms.worker.loader;
 
 import com.courage.platform.sms.adapter.OuterAdapter;
 import com.courage.platform.sms.adapter.support.ExtensionLoader;
+import com.courage.platform.sms.adapter.support.SmsChannelConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +16,21 @@ public class SmsAdapterLoader {
     private ExtensionLoader<OuterAdapter> extensionLoader;
 
     public void init() throws Exception {
-        this.extensionLoader = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
+        extensionLoader = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
+    }
+
+    private void loadAdapter(String adapterName, SmsChannelConfig smsChannelConfig) {
+        try {
+            OuterAdapter adapter = extensionLoader.getExtension(adapterName);
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            // 替换ClassLoader
+            Thread.currentThread().setContextClassLoader(adapter.getClass().getClassLoader());
+            adapter.init(smsChannelConfig);
+            Thread.currentThread().setContextClassLoader(cl);
+            logger.info("Load sms adapter: {} succeed", adapterName);
+        } catch (Exception e) {
+            logger.error("Load canal adapter: {} failed", adapterName, e);
+        }
     }
 
     public void destroy() {
