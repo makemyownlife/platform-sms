8758e05ce27c1fb7882be5110e6988ecfc816505
zhangyong <zhangyong7120180@163.com>
2023-02-10 17:17:41 +0800

    [修改代码] adapter 服务

--- platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterService.java
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/loader/SmsAdapterService.java
@@ -1,6 +1,5 @@
 package com.courage.platform.sms.worker.loader;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
