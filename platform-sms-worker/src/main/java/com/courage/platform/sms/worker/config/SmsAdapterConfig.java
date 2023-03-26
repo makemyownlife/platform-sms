0f43f59b418c499fb1d1c4f8311f488f7b53982b
makemyownlife <zhangyong7120180@163.com>
2023-03-26 21:40:01 +0800

    [添加代码] 正常的查询

--- platform-sms-worker/src/main/java/com/courage/platform/sms/worker/config/SmsAdapterConfig.java
+++ platform-sms-worker/src/main/java/com/courage/platform/sms/worker/config/SmsAdapterConfig.java
@@ -3,7 +3,6 @@ package com.courage.platform.sms.worker.config;
 import com.courage.platform.sms.adapter.support.SmsChannelConfig;
 import com.courage.platform.sms.dao.TSmsChannelDAO;
 import com.courage.platform.sms.domain.TSmsChannel;
-import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,6 +24,7 @@ public class SmsAdapterConfig {
     public List<SmsChannelConfig> getChannelConfigs() {
         List<SmsChannelConfig> channelConfigs = new ArrayList<SmsChannelConfig>();
         Map<String, String> param = new HashMap<String, String>();
+        //正常的渠道
         param.put("status", "0");
         List<TSmsChannel> tSmsChannels = tSmsChannelDAO.queryChannels(param);
         for (TSmsChannel tSmsChannel : tSmsChannels) {
