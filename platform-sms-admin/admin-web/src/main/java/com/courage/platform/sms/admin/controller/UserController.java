6babb13a07f685878401f9c82ec72e33275c0a58
makemyownlife <zhangyong7120180@163.com>
2023-03-26 17:34:50 +0800

    [添加代码] 用户登录做完

--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/controller/UserController.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/controller/UserController.java
@@ -2,13 +2,17 @@ package com.courage.platform.sms.admin.controller;
 
 import com.courage.platform.sms.admin.model.AdminUser;
 import com.courage.platform.sms.admin.model.BaseModel;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -16,20 +20,44 @@ public class UserController {
 
     private final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    public static final LoadingCache<String, AdminUser> loginUsers = Caffeine.newBuilder()
+            .maximumSize(10_000)
+            .expireAfterAccess(20, TimeUnit.MINUTES)
+            .build(key -> null); // 用户登录信息缓存
+
+    @Value("${sms.adminUser}")
+    private String adminUser;
+
+    @Value("${sms.adminPasswd}")
+    private String adminPasswd;
+
     @PostMapping(value = "/login")
     public BaseModel<Map<String, String>> login(@RequestBody AdminUser user) {
         Map<String, String> tokenResp = new HashMap<>();
-        String token = UUID.randomUUID().toString();
-        tokenResp.put("token", token);
-        return BaseModel.getInstance(tokenResp);
+        if (user.getUsername().equals(adminUser) && user.getPassword().equals(adminPasswd)) {
+            String token = UUID.randomUUID().toString();
+            tokenResp.put("token", token);
+            loginUsers.put(token, user);
+            return BaseModel.getInstance(tokenResp);
+        } else {
+            BaseModel<Map<String, String>> model = BaseModel.getInstance(null);
+            model.setCode(40001);
+            model.setMessage("Invalid username or password");
+            return model;
+        }
     }
 
     @GetMapping(value = "/info")
     public BaseModel info(@RequestParam String token) {
-        logger.info("token:" + token);
-        AdminUser adminUser = new AdminUser();
-        adminUser.setUsername("makemyownlife");
-        return BaseModel.getInstance(adminUser);
+        AdminUser user = loginUsers.getIfPresent(token);
+        if (user != null) {
+            return BaseModel.getInstance(user);
+        } else {
+            BaseModel<AdminUser> model = BaseModel.getInstance(null);
+            model.setCode(50014);
+            model.setMessage("Invalid token");
+            return model;
+        }
     }
 
 }
