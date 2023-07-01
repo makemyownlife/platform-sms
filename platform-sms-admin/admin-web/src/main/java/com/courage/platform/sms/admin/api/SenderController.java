d5dcb6bc5bbd56db2e336873d90202e72929ad8e
zhangyong <zhangyong7120180@163.com>
2023-07-01 16:51:02 +0800

    [修改代码] 优化短信平台模式

--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/api/SenderController.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/api/SenderController.java
@@ -1,15 +1,9 @@
-package com.courage.platform.sms.server.controller;
+package com.courage.platform.sms.admin.api;
 
 import com.courage.platform.sms.client.SmsSenderResult;
-import org.apache.rocketmq.client.producer.SendResult;
-import org.apache.rocketmq.client.producer.SendStatus;
-import org.apache.rocketmq.common.message.MessageConst;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,9 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 public class SenderController {
 
     private final static Logger logger = LoggerFactory.getLogger(SenderController.class);
-
-    @Autowired
-    private RocketMQTemplate rocketMQTemplate;
 
     @Value("${rocketmq.topic}")
     private String smsTopic;
@@ -42,16 +33,6 @@ public class SenderController {
             logger.info("q:" + q + " appKey:" + appKey + " uniqueId:" + uniqueId);
             //发送消息
             String tag = "template";
-            SendResult sendResult = rocketMQTemplate.syncSend(
-                    smsTopic + ":" + tag,
-                    //链式调用
-                    MessageBuilder.withPayload(q).setHeader(MessageConst.PROPERTY_KEYS, uniqueId).build());
-            logger.info("uniqueId：" + uniqueId + " sendResult:" + sendResult);
-            if (sendResult != null) {
-                if (sendResult.getSendStatus() == SendStatus.SEND_OK) {
-                    return new SmsSenderResult(sendResult.getMsgId());
-                }
-            }
             return new SmsSenderResult(SmsSenderResult.FAIL_CODE, "发送失败");
         } catch (Exception e) {
             logger.error("sendSingle error: ", e);
