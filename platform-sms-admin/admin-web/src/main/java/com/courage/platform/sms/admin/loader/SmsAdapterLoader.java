7db81866388cf40897c4047a6f8b906fa2c72210
zhangyong <zhangyong7120180@163.com>
2023-07-09 19:32:01 +0800

    合理规划模块

--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/loader/SmsAdapterLoader.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/loader/SmsAdapterLoader.java
@@ -1,8 +1,34 @@
 package com.courage.platform.sms.admin.loader;
 
+import com.courage.platform.sms.adapter.OuterAdapter;
+import com.courage.platform.sms.adapter.support.ExtensionLoader;
+import com.courage.platform.sms.adapter.support.SmsChannelConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 /**
  * 适配器加载器
  */
+@Component
 public class SmsAdapterLoader {
+
+    private final static Logger logger = LoggerFactory.getLogger(SmsAdapterLoader.class);
+
+    private static ExtensionLoader<OuterAdapter> EXTENSION_LOADER = ExtensionLoader.getExtensionLoader(OuterAdapter.class);
+
+    private void loadAdapter(String adapterName, SmsChannelConfig smsChannelConfig) {
+        try {
+            OuterAdapter adapter = EXTENSION_LOADER.getExtension(adapterName);
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            // 替换ClassLoader
+            Thread.currentThread().setContextClassLoader(adapter.getClass().getClassLoader());
+            adapter.init(smsChannelConfig);
+            Thread.currentThread().setContextClassLoader(cl);
+            logger.info("Load sms adapter: {} succeed", adapterName);
+        } catch (Exception e) {
+            logger.error("Load canal adapter: {} failed", adapterName, e);
+        }
+    }
 
 }
--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/loader/SmsAdatperProcessor.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/loader/SmsAdatperProcessor.java
@@ -2,11 +2,13 @@ package com.courage.platform.sms.admin.loader;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * 适配器处理器
  * Created by zhangyong on 2023/5/5.
  */
+@Component
 public class SmsAdatperProcessor {
 
     private final static Logger logger = LoggerFactory.getLogger(SmsAdatperProcessor.class);
