f994779b4f49805270383d803931edc61780b332
zhangyong <zhangyong7120180@163.com>
2023-07-27 20:26:40 +0800

    短信发送信息

--- platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/domain/TSmsRecord.java
+++ platform-sms-admin/admin-web/src/main/java/com/courage/platform/sms/admin/domain/TSmsRecord.java
@@ -4,15 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class TSmsRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     /**
      * 主键 自增
      */
     private Long id;
 
     /**
-     * 短信id
+     * 手机号
      */
-    private String smsId;
+    private String mobile;
+
 
     /**
      * 区位码
@@ -24,20 +28,16 @@ public class TSmsRecord implements Serializable {
      */
     private String appId;
 
-    /**
-     * 短信内容
-     */
-    private String content;
 
     /**
-     * 0：普通短信  1 ：营销短信 （群发）
+     * 0：普通短信 / 1 ：营销短信 （群发）
      */
-    private Byte type;
+    private Integer recordType;
 
     /**
      * 渠道
      */
-    private String channel;
+    private Integer channelId;
 
     /**
      * 指定发送时间
@@ -47,20 +47,13 @@ public class TSmsRecord implements Serializable {
     /**
      * -1：待发送 / 0：已发送  /1  : 发送失败
      */
-    private Byte sendStatus;
+    private Integer sendStatus;
 
     private String senderIp;
 
     private Date createTime;
 
     private Date updateTime;
-
-    /**
-     * 手机号
-     */
-    private String mobile;
-
-    private static final long serialVersionUID = 1L;
 
     public Long getId() {
         return id;
@@ -70,12 +63,12 @@ public class TSmsRecord implements Serializable {
         this.id = id;
     }
 
-    public String getSmsId() {
-        return smsId;
+    public String getMobile() {
+        return mobile;
     }
 
-    public void setSmsId(String smsId) {
-        this.smsId = smsId;
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
     }
 
     public String getNationcode() {
@@ -94,28 +87,12 @@ public class TSmsRecord implements Serializable {
         this.appId = appId;
     }
 
-    public String getContent() {
-        return content;
+    public Integer getChannelId() {
+        return channelId;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Byte getType() {
-        return type;
-    }
-
-    public void setType(Byte type) {
-        this.type = type;
-    }
-
-    public String getChannel() {
-        return channel;
-    }
-
-    public void setChannel(String channel) {
-        this.channel = channel;
+    public void setChannelId(Integer channelId) {
+        this.channelId = channelId;
     }
 
     public String getAttime() {
@@ -126,11 +103,11 @@ public class TSmsRecord implements Serializable {
         this.attime = attime;
     }
 
-    public Byte getSendStatus() {
+    public Integer getSendStatus() {
         return sendStatus;
     }
 
-    public void setSendStatus(Byte sendStatus) {
+    public void setSendStatus(Integer sendStatus) {
         this.sendStatus = sendStatus;
     }
 
@@ -158,82 +135,12 @@ public class TSmsRecord implements Serializable {
         this.updateTime = updateTime;
     }
 
-    public String getMobile() {
-        return mobile;
+    public Integer getRecordType() {
+        return recordType;
     }
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
+    public void setRecordType(Integer recordType) {
+        this.recordType = recordType;
     }
 
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        TSmsRecord other = (TSmsRecord) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getSmsId() == null ? other.getSmsId() == null : this.getSmsId().equals(other.getSmsId()))
-            && (this.getNationcode() == null ? other.getNationcode() == null : this.getNationcode().equals(other.getNationcode()))
-            && (this.getAppId() == null ? other.getAppId() == null : this.getAppId().equals(other.getAppId()))
-            && (this.getContent() == null ? other.getContent() == null : this.getContent().equals(other.getContent()))
-            && (this.getType() == null ? other.getType() == null : this.getType().equals(other.getType()))
-            && (this.getChannel() == null ? other.getChannel() == null : this.getChannel().equals(other.getChannel()))
-            && (this.getAttime() == null ? other.getAttime() == null : this.getAttime().equals(other.getAttime()))
-            && (this.getSendStatus() == null ? other.getSendStatus() == null : this.getSendStatus().equals(other.getSendStatus()))
-            && (this.getSenderIp() == null ? other.getSenderIp() == null : this.getSenderIp().equals(other.getSenderIp()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
-            && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()))
-            && (this.getMobile() == null ? other.getMobile() == null : this.getMobile().equals(other.getMobile()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getSmsId() == null) ? 0 : getSmsId().hashCode());
-        result = prime * result + ((getNationcode() == null) ? 0 : getNationcode().hashCode());
-        result = prime * result + ((getAppId() == null) ? 0 : getAppId().hashCode());
-        result = prime * result + ((getContent() == null) ? 0 : getContent().hashCode());
-        result = prime * result + ((getType() == null) ? 0 : getType().hashCode());
-        result = prime * result + ((getChannel() == null) ? 0 : getChannel().hashCode());
-        result = prime * result + ((getAttime() == null) ? 0 : getAttime().hashCode());
-        result = prime * result + ((getSendStatus() == null) ? 0 : getSendStatus().hashCode());
-        result = prime * result + ((getSenderIp() == null) ? 0 : getSenderIp().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
-        result = prime * result + ((getMobile() == null) ? 0 : getMobile().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", smsId=").append(smsId);
-        sb.append(", nationcode=").append(nationcode);
-        sb.append(", appId=").append(appId);
-        sb.append(", content=").append(content);
-        sb.append(", type=").append(type);
-        sb.append(", channel=").append(channel);
-        sb.append(", attime=").append(attime);
-        sb.append(", sendStatus=").append(sendStatus);
-        sb.append(", senderIp=").append(senderIp);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", mobile=").append(mobile);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
