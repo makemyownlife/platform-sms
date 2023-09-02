993532cf861e4ceff3a8767487c5be6f4a5c3f66
makemyownlife <zhangyong7120180@163.com>
2023-09-02 19:01:08 +0800

    [添加代码] 测试内容是可以的

--- platform-sms-adapter/adapter-emay/src/main/java/com/courage/platform/sms/adapter/emay/EmayAdapter.java
+++ platform-sms-adapter/adapter-emay/src/main/java/com/courage/platform/sms/adapter/emay/EmayAdapter.java
@@ -1,12 +1,5 @@
 package com.courage.platform.sms.adapter.emay;
 
-import cn.emay.ResultModel;
-import cn.emay.eucp.inter.framework.dto.TemplateSmsIdAndMobile;
-import cn.emay.eucp.inter.http.v1.dto.request.TemplateSmsSendRequest;
-import cn.emay.util.HttpUtil;
-import cn.emay.util.JsonHelper;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.courage.platform.sms.adapter.OuterAdapter;
 import com.courage.platform.sms.adapter.command.AddSmsTemplateCommand;
 import com.courage.platform.sms.adapter.command.SendSmsCommand;
@@ -38,56 +31,17 @@ public class EmayAdapter implements OuterAdapter {
     public void init(SmsChannelConfig smsChannelConfig) {
         logger.info("初始化亿美短信客户端 渠道编号:[" + smsChannelConfig.getId() + "] appkey:[" + smsChannelConfig.getChannelAppkey() + "]");
         this.smsChannelConfig = smsChannelConfig;
+
     }
 
     @Override
     public SmsResponseCommand sendSmsByTemplateId(SendSmsCommand smsSendRequest) {
-        TemplateSmsSendRequest pamars = new TemplateSmsSendRequest();
-        String[] mobiles = StringUtils.split(smsSendRequest.getPhoneNumbers(), ",");
-        List<TemplateSmsIdAndMobile> smsIdAndMobilesList = new ArrayList(mobiles.length);
-        for (int i = 0; i < mobiles.length; i++) {
-            TemplateSmsIdAndMobile templateSmsIdAndMobile = new TemplateSmsIdAndMobile(mobiles[i], UUID.randomUUID().toString().replaceAll("-", ""));
-            smsIdAndMobilesList.add(templateSmsIdAndMobile);
-        }
-        pamars.setTemplateId(smsSendRequest.getTemplateCode());
-        pamars.setExtendedCode(extendCode);
-        pamars.setRequestTime(System.currentTimeMillis());
-        pamars.setTimerTime(smsSendRequest.getTimerTime());
-        logger.info(JsonHelper.toJsonString(pamars));
-        ResultModel result = HttpUtil.request(smsChannelConfig.getChannelAppkey(), smsChannelConfig.getChannelAppsecret(), algorithm, pamars, smsChannelConfig.getChannelDomain() + "/inter/sendTemplateNormalSMS", isGizp, encode);
-        if ("SUCCESS".equals(result.getCode())) {
-            cn.emay.eucp.inter.http.v1.dto.response.SmsResponse[] response = JsonHelper.fromJson(cn.emay.eucp.inter.http.v1.dto.response.SmsResponse[].class, result.getResult());
-            if (response != null) {
-                for (cn.emay.eucp.inter.http.v1.dto.response.SmsResponse d : response) {
-                    logger.info("data:" + d.getMobile() + "," + d.getSmsId() + "," + d.getCustomSmsId());
-                }
-                return new SmsResponseCommand(SmsResponseCommand.SUCCESS_CODE, result.getResult());
-            }
-        }
-        return new SmsResponseCommand(SmsResponseCommand.FAIL_CODE, result.getResult());
+        return null;
     }
 
     @Override
     public SmsResponseCommand addSmsTemplate(AddSmsTemplateCommand addSmsTemplateCommand) {
-        // 将标准模版转换成 亿美模版 信息
-        String templateContent = addSmsTemplateCommand.getTemplateContent();
-        Map<String, String> templateMap = new HashMap<String, String>();
-        templateMap.put("templateContent", templateContent);
-        templateMap.put("requestTime", String.valueOf(System.currentTimeMillis()));
-        templateMap.put("requestValidPeriod", "30");
-        ResultModel result = HttpUtil.request(smsChannelConfig.getChannelAppkey(), smsChannelConfig.getChannelAppsecret(), algorithm, templateMap, smsChannelConfig.getChannelDomain() + "/inter/createTemplateSMS", isGizp, encode);
-        logger.info("result:" + result.getResult());
-        if ("SUCCESS".equals(result.getCode())) {
-            // {"templateId":"168984611698400172"}
-            JSONObject templateIdJSON = JSON.parseObject(result.getResult());
-            Map<String, String> bodyMap = new HashMap<>();
-            bodyMap.put("templateCode", templateIdJSON.getString("templateId"));
-            bodyMap.put("templateContent", templateContent);
-            // 0 待提交:  1：待审核  2：审核成功 3：审核失败 亿美提交申请默认成功
-            bodyMap.put("status", "2");
-            return new SmsResponseCommand(SmsResponseCommand.SUCCESS_CODE, bodyMap);
-        }
-        return new SmsResponseCommand(SmsResponseCommand.FAIL_CODE);
+        return null;
     }
 
     @Override
--- platform-sms-adapter/adapter-emay/src/test/java/com/courage/platform/sms/adapter/emay/test/EmayUnitTest.java
+++ platform-sms-adapter/adapter-emay/src/test/java/com/courage/platform/sms/adapter/emay/test/EmayUnitTest.java
@@ -1,13 +1,10 @@
 package com.courage.platform.sms.adapter.emay.test;
 
-import cn.emay.ResultModel;
-import cn.emay.eucp.inter.framework.dto.TemplateSmsIdAndMobile;
-import cn.emay.eucp.inter.http.v1.dto.request.TemplateSmsSendRequest;
-import cn.emay.eucp.inter.http.v1.dto.response.SmsResponse;
-import cn.emay.util.AES;
-import cn.emay.util.GZIPUtils;
-import cn.emay.util.JsonHelper;
-import cn.emay.util.http.*;
+import cn.emay.sdk.client.SmsSDKClient;
+import cn.emay.sdk.core.dto.sms.common.ResultModel;
+import cn.emay.sdk.core.dto.sms.request.SmsSingleRequest;
+import cn.emay.sdk.core.dto.sms.response.SmsResponse;
+import cn.emay.sdk.util.exception.SDKParamsException;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -16,172 +13,26 @@ import java.util.Map;
 public class EmayUnitTest {
 
     @Test
-    public void sendSingleSMS() {
+    public void sendSingleSMS() throws SDKParamsException {
         String appId = "EUCP-EMY-SMS1-0BIGB";// 请联系销售，或者在页面中 获取
         // 密钥
         String secretKey = "69C5DCC5531E15BF";// 请联系销售，或者在页面中 获取
         // 接口地址
-        String host = "http://www.btom.cn:8080"; // 请联系销售获取
-        // 加密算法
-        String algorithm = "AES/ECB/PKCS5Padding";
-        // 编码
-        String encode = "UTF-8";
-        // 是否压缩
-        boolean isGizp = true;
-        String templateId = "22222";
-        // 扩展 code
-        String extendedCode = "111";
-        TemplateSmsSendRequest pamars = new TemplateSmsSendRequest();
-        TemplateSmsIdAndMobile[] customSmsIdAndMobiles =
-                new TemplateSmsIdAndMobile[]{new TemplateSmsIdAndMobile("15011319235", "1"),
-                        new TemplateSmsIdAndMobile("17607197036", "2")};
-        pamars.setSmses(customSmsIdAndMobiles);
-        pamars.setTemplateId(templateId);
-        pamars.setExtendedCode(extendedCode);
-        pamars.setRequestTime(System.currentTimeMillis());
-        System.out.println(JsonHelper.toJsonString(pamars));
-        ResultModel result = request(appId, secretKey, algorithm, pamars, host + "/inter/sendTemplateNormalSMS", isGizp, encode);
-        System.out.println("result code :" + result.getCode());
-        if ("SUCCESS".equals(result.getCode())) {
-            SmsResponse[] response = JsonHelper.fromJson(SmsResponse[].class, result.getResult());
-            if (response != null) {
-                for (SmsResponse d : response) {
-                    System.out.println("data:" + d.getMobile() + "," + d.getSmsId() + "," + d.getCustomSmsId());
-                }
-            }
+        String host = "http://www.btom.cn"; // 请联系销售获取
+        SmsSDKClient client = new SmsSDKClient(host, 8080, appId, secretKey);
+        String mobile = "15011319235";
+        String content = "【张勇的博客】短信内容";
+        String customSmsId = "1";
+        String extendedCode = "01";
+        SmsSingleRequest request = new SmsSingleRequest(mobile, content, customSmsId, extendedCode, "");
+        ResultModel<SmsResponse> result = client.sendSingleSms(request);
+        if (result.getCode().equals("SUCCESS")) {
+            System.out.println("请求成功");
+            SmsResponse response = result.getResult();
+            System.out.println("sendSingleSms:" + response.toString());
+        } else {
+            System.out.println("请求失败");
         }
-    }
-
-    @Test
-    public void sendTemplate() {
-        String appId = "EUCP-EMY-SMS1-0BIGB";// 请联系销售，或者在页面中 获取
-        // 密钥
-        String secretKey = "69C5DCC5531E15BF";// 请联系销售，或者在页面中 获取
-        // 接口地址
-        String host = "http://www.btom.cn:8080"; // 请联系销售获取
-        // 加密算法
-        String algorithm = "AES/ECB/PKCS5Padding";
-        // 编码
-        String encode = "UTF-8";
-        // 是否压缩
-        boolean isGizp = true;
-        String templateId = "22222";
-        // 扩展 code
-        String extendedCode = "111";
-        TemplateSmsSendRequest pamars = new TemplateSmsSendRequest();
-        TemplateSmsIdAndMobile[] customSmsIdAndMobiles =
-                new TemplateSmsIdAndMobile[]{new TemplateSmsIdAndMobile("15011319235", "1"),
-                        new TemplateSmsIdAndMobile("17607197036", "2")};
-        pamars.setSmses(customSmsIdAndMobiles);
-        pamars.setTemplateId(templateId);
-        pamars.setExtendedCode(extendedCode);
-        pamars.setRequestTime(System.currentTimeMillis());
-        System.out.println(JsonHelper.toJsonString(pamars));
-        ResultModel result = request(appId, secretKey, algorithm, pamars, host + "/inter/sendTemplateNormalSMS", isGizp, encode);
-        System.out.println("result code :" + result.getCode());
-        if ("SUCCESS".equals(result.getCode())) {
-            SmsResponse[] response = JsonHelper.fromJson(SmsResponse[].class, result.getResult());
-            if (response != null) {
-                for (SmsResponse d : response) {
-                    System.out.println("data:" + d.getMobile() + "," + d.getSmsId() + "," + d.getCustomSmsId());
-                }
-            }
-        }
-    }
-
-    @Test
-    public void addTemplate() {
-        String appId = "EUCP-EMY-SMS1-0BIGB";// 请联系销售，或者在页面中 获取
-        // 密钥
-        String secretKey = "69C5DCC5531E15BF";// 请联系销售，或者在页面中 获取
-        // 接口地址
-        String host = "http://www.btom.cn:8080"; // 请联系销售获取
-        // 加密算法
-        String algorithm = "AES/ECB/PKCS5Padding";
-        // 编码
-        String encode = "UTF-8";
-        // 是否压缩
-        boolean isGizp = true;
-        // 模板创建
-        Map<String, String> templateMap = new HashMap<String, String>();
-        templateMap.put("templateContent", "ssssssssss${参数1}dddddddddddddd${参数2}dddddddddd");
-        templateMap.put("requestTime", String.valueOf(System.currentTimeMillis()));
-        templateMap.put("requestValidPeriod", "30");
-        ResultModel result = request(appId, secretKey, algorithm, templateMap, host + "/inter/createTemplateSMS", isGizp, encode);
-        System.out.println("result code :" + result.getCode());
-        if ("SUCCESS".equals(result.getCode())) {
-            System.out.println(result.getResult());
-        }
-        System.out.println("=============end createTemplateSMS==================");
-    }
-
-    public static ResultModel request(String appId, String secretKey, String algorithm, Object content, String url, final boolean isGzip, String encode) {
-        Map<String, String> headers = new HashMap<String, String>();
-        HttpRequest<byte[]> request = null;
-        try {
-            headers.put("appId", appId);
-            headers.put("encode", encode);
-            String requestJson = JsonHelper.toJsonString(content);
-            System.out.println("result json: " + requestJson);
-            byte[] bytes = requestJson.getBytes(encode);
-            System.out.println("request data size : " + bytes.length);
-            if (isGzip) {
-                headers.put("gzip", "on");
-                bytes = GZIPUtils.compress(bytes);
-                System.out.println("request data size [com]: " + bytes.length);
-            }
-            byte[] parambytes = AES.encrypt(bytes, secretKey.getBytes(), algorithm);
-            System.out.println("request data size [en] : " + parambytes.length);
-            HttpRequestParams<byte[]> params = new HttpRequestParams<byte[]>();
-            params.setCharSet("UTF-8");
-            params.setMethod("POST");
-            params.setHeaders(headers);
-            params.setParams(parambytes);
-            params.setUrl(url);
-            if (url.startsWith("https://")) {
-                request = new HttpsRequestBytes(params, null);
-            } else {
-                request = new HttpRequestBytes(params);
-            }
-        } catch (Exception e) {
-            System.out.println("加密异常");
-            e.printStackTrace();
-        }
-        HttpClient client = new HttpClient();
-        String code = null;
-        String result = null;
-        try {
-            HttpResponseBytes res = client.service(request, new HttpResponseBytesPraser());
-            if (res == null) {
-                System.out.println("请求接口异常");
-                return new ResultModel(code, result);
-            }
-            if (res.getResultCode().equals(HttpResultCode.SUCCESS)) {
-                if (res.getHttpCode() == 200) {
-                    code = res.getHeaders().get("result");
-                    if (code.equals("SUCCESS")) {
-                        byte[] data = res.getResult();
-                        System.out.println("response data size [en and com] : " + data.length);
-                        data = AES.decrypt(data, secretKey.getBytes(), algorithm);
-                        if (isGzip) {
-                            data = GZIPUtils.decompress(data);
-                        }
-                        System.out.println("response data size : " + data.length);
-                        result = new String(data, encode);
-                        System.out.println("response json: " + result);
-                    }
-                } else {
-                    System.out.println("请求接口异常,请求码:" + res.getHttpCode());
-                }
-            } else {
-                System.out.println("请求接口网络异常:" + res.getResultCode().getCode());
-            }
-        } catch (Exception e) {
-            System.out.println("解析失败");
-            e.printStackTrace();
-        }
-        ResultModel re = new ResultModel(code, result);
-        return re;
     }
 
 }
