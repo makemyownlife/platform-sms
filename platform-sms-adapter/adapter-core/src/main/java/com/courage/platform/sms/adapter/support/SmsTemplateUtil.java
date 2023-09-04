19d2b0e1a0ec46e9e298a825aaf515601de3e42d
makemyownlife <zhangyong7120180@163.com>
2023-09-04 15:56:17 +0800

    [添加代码] 亿美短信优化

---
+++ platform-sms-adapter/adapter-core/src/main/java/com/courage/platform/sms/adapter/support/SmsTemplateUtil.java
@@ -0,0 +1,37 @@
+package com.courage.platform.sms.adapter.support;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 短信模版工具类
+ */
+public class SmsTemplateUtil {
+
+    public static String renderContentWithSignName(String templateParam, String templateContent, String signName) {
+        Map<String, String> templateParamMap = JSON.parseObject(templateParam, HashMap.class);
+        return renderContentWithSignName(templateParamMap, templateContent, signName);
+    }
+
+    /**
+     * 将模版渲染成短信内容
+     *
+     * @param templateParam   模版参数
+     * @param templateContent 模版内容
+     * @return 短信内容(不包含签名)
+     */
+    public static String renderContentWithSignName(Map<String, String> templateParam, String templateContent, String signName) {
+        String result = StringUtils.EMPTY;
+        String prefix = StringUtils.isEmpty(signName) ? StringUtils.EMPTY : "【" + signName + "】";
+        // 遍历模板中的参数占位符，并替换为对应的参数值
+        for (Map.Entry<String, String> entry : templateParam.entrySet()) {
+            String placeholder = "${" + entry.getKey() + "}";
+            String paramValue = entry.getValue();
+            result = result.replace(placeholder, paramValue);
+        }
+        return prefix + result;
+    }
+}
--- platform-sms-adapter/adapter-emay/src/main/java/cn/emay/sdk/core/dto/sms/response/SmsResponse.java
+++ platform-sms-adapter/adapter-emay/src/main/java/cn/emay/sdk/core/dto/sms/response/SmsResponse.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 public class SmsResponse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	/**
 	 * 系统唯一smsId
 	 */
