d05e28c74fee2834200487f9643204d98ec07aa9
makemyownlife <zhangyong7120180@163.com>
2023-09-08 00:31:16 +0800

    [添加代码] 腾讯配置信息

--- platform-sms-adapter/adapter-tencent/src/main/java/com/courage/platform/sms/adapter/tencent/TencentAdapter.java
+++ platform-sms-adapter/adapter-tencent/src/main/java/com/courage/platform/sms/adapter/tencent/TencentAdapter.java
@@ -7,6 +7,10 @@ import com.courage.platform.sms.adapter.command.request.SendSmsCommand;
 import com.courage.platform.sms.adapter.command.response.SmsResponseCommand;
 import com.courage.platform.sms.adapter.support.SPI;
 import com.courage.platform.sms.adapter.support.SmsChannelConfig;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.profile.ClientProfile;
+import com.tencentcloudapi.common.profile.HttpProfile;
+import com.tencentcloudapi.sms.v20190711.SmsClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,10 +23,21 @@ public class TencentAdapter implements OuterAdapter {
 
     private SmsChannelConfig smsChannelConfig;
 
+    private SmsClient client;
+
     @Override
     public void init(SmsChannelConfig smsChannelConfig) {
         logger.info("初始化腾讯短信客户端 渠道编号:[" + smsChannelConfig.getId() + "] appkey:[" + smsChannelConfig.getChannelAppkey() + "]");
         this.smsChannelConfig = smsChannelConfig;
+        Credential cred = new Credential(smsChannelConfig.getChannelAppkey(), smsChannelConfig.getChannelAppsecret());
+        HttpProfile httpProfile = new HttpProfile();
+        httpProfile.setReqMethod("POST");
+        httpProfile.setConnTimeout(60);
+        httpProfile.setEndpoint("sms.tencentcloudapi.com");
+        ClientProfile clientProfile = new ClientProfile();
+        clientProfile.setSignMethod("HmacSHA256");
+        clientProfile.setHttpProfile(httpProfile);
+        this.client = new SmsClient(cred, "ap-guangzhou", clientProfile);
     }
 
     @Override
