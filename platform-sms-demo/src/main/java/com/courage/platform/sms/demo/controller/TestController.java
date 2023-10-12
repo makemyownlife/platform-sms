aa85288def0263bac7673a3db35978af773c49d3
makemyownlife <zhangyong7120180@163.com>
2023-10-12 18:28:41 +0800

    [添加代码] TestController

--- platform-sms-demo/src/main/java/com/courage/platform/sms/demo/controller/TestController.java
+++ platform-sms-demo/src/main/java/com/courage/platform/sms/demo/controller/TestController.java
@@ -5,7 +5,6 @@ import com.courage.platform.sms.client.SmsSenderClient;
 import com.courage.platform.sms.client.SmsSenderResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
