99ec85819e7239dd5aea16369215ef772f804620
zhangyong <zhangyong7120180@163.com>
2023-10-12 16:17:28 +0800

    短信配置信息

---
+++ platform-sms-demo/src/main/java/com/courage/platform/sms/demo/config/SmsConfiguration.java
@@ -0,0 +1,14 @@
+package com.courage.platform.sms.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhangyong on 2023/10/12.
+ */
+@Configuration
+public class SmsConfiguration {
+
+
+
+}
