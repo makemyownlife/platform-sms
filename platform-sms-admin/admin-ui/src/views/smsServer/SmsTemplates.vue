f13eefcdf55494b8b5a6fe5d9561ecad333c405f
zhangyong <zhangyong7120180@163.com>
2023-10-28 17:42:01 +0800

    [修改代码] 去掉自动绑定

--- platform-sms-admin/admin-ui/src/views/smsServer/SmsTemplates.vue
+++ platform-sms-admin/admin-ui/src/views/smsServer/SmsTemplates.vue
@@ -69,9 +69,9 @@
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item @click.native="handleUpdate(scope.row)">修改模版</el-dropdown-item>
               <el-dropdown-item @click.native="handBinding(scope.row)">手工绑定</el-dropdown-item>
-              <!-- 暂时屏蔽掉 自动绑定  start -->
+              <!-- 暂时屏蔽掉 自动绑定
               <el-dropdown-item @click.native="autoBinding(scope.row)">自动绑定</el-dropdown-item>
-              <!-- 暂时屏蔽掉 自动绑定  end -->
+              暂时屏蔽掉 自动绑定  end -->
             </el-dropdown-menu>
         </el-dropdown>
       </template>
