32fb07107c2dee6a8cede91d7d718e72be028f69
makemyownlife <zhangyong7120180@163.com>
2023-11-23 07:57:06 +0800

    SPI 学习

--- platform-sms-adapter/adapter-core/src/test/java/com/courage/platform/sms/adapter/test/javaspi/Bumblebee.java
+++ platform-sms-adapter/adapter-core/src/test/java/com/courage/platform/sms/adapter/test/javaspi/Bumblebee.java
@@ -1,6 +1,7 @@
 package com.courage.platform.sms.adapter.test.javaspi;
 
 public class Bumblebee implements Robot {
+
     @Override
     public void sayHello() {
         System.out.println("Hello, I am Bumblebee.");
