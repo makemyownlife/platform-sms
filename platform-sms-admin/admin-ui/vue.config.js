a85adf0d3fc14f931ff162eb4ce5790632d38f8d
zhangyong <zhangyong7120180@163.com>
2024-05-10 11:49:40 +0800

    [优化代码] 调整 版本 至 0.6.0 ，同时修改 ui 相对路径

--- platform-sms-admin/admin-ui/vue.config.js
+++ platform-sms-admin/admin-ui/vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'target/dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
